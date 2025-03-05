8b0c9ddd863c35def120fd85d3bbbfa2129c25eb
Ashish Farande <ashish.farande@gmail.com>
2025-03-05 06:46:51 -0700

    Create span from an array

--- Logger/TypeDescriptors.h
+++ Logger/TypeDescriptors.h
@@ -18,10 +18,40 @@ struct CStr
 };
 
 
-using TypeDescriptor = std::variant<Int, Float, CStr>;
+template <typename T>
+class Span {
+public:
+    // Constructors
+    template <std::size_t N>
+    constexpr Span(std::array<T, N>& arr) noexcept
+        : data_(arr.data()), size_(N) {}
 
-template <size_t N>
-using TypeDescriptors = std::array<TypeDescriptor, N>;
+    template <std::size_t N>
+    constexpr Span(const std::array<T, N>& arr) noexcept
+        : data_(arr.data()), size_(N) {}
+
+    // Getter functions
+    constexpr T* data() noexcept { return data_; }
+    constexpr const T* data() const noexcept { return data_; }
+    constexpr std::size_t size() const noexcept { return size_; }
+
+    // Indexing
+    constexpr T& operator[](std::size_t index) { return data_[index]; }
+    constexpr const T& operator[](std::size_t index) const { return data_[index]; }
+
+    // Iterators
+    constexpr T* begin() noexcept { return data_; }
+    constexpr const T* begin() const noexcept { return data_; }
+    constexpr T* end() noexcept { return data_ + size_; }
+    constexpr const T* end() const noexcept { return data_ + size_; }
+
+private:
+    const T* data_;
+    std::size_t size_;
+};
+
+using TypeDescriptor = std::variant<Int, Float, CStr>;
+using TypeDescriptors = Span<TypeDescriptor>;
 
 template <typename T>
 struct GetTypeDescriptor;
--- Logger/meta_data.h
+++ Logger/meta_data.h
@@ -29,23 +29,22 @@ struct MetaData
     Level level;
 };
 
-template <size_t N>
 struct MetaDataWithDescriptors
 {
-    MetaData macroData{};
-    TypeDescriptors<N> desciprtors{};
+    MetaData macroData;
+    TypeDescriptors desciprtors;
 };
 
 struct MetaDataNode
 {
-    constexpr MetaDataNode(const void *d) : id(gen_id()), data(d)
+    constexpr MetaDataNode(const MetaDataWithDescriptors *d) : id(gen_id()), data(d)
     {
         auto &head = head_node();
         next = std::exchange(head, this);
     }
 
     int64_t id;
-    const void *data{nullptr};
+    const MetaDataWithDescriptors *data{nullptr};
     MetaDataNode *next{nullptr};
 };
 
@@ -53,7 +52,7 @@ template <class MetaData_, class... Args>
 constexpr auto get_meta_data_ptr()
 {
     static constexpr std::array type_descriptors{GetTypeDescriptor<Args>::value...};
-    static constexpr MetaDataWithDescriptors meta_data{MetaData_{}(), type_descriptors};
+    static constexpr MetaDataWithDescriptors meta_data{MetaData_{}(), Span{type_descriptors}};
     return &meta_data;
 }
 
